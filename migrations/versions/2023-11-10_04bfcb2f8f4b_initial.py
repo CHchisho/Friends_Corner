cd0a3610a7251cde4e8a382c55abd863a1c851ef
Ilia Chebanik <iliebanik@gmail.com>
2024-01-03 21:59:47 +0200

    Try to deploy 8

--- migrations/versions/2023-11-10_04bfcb2f8f4b_initial.py
+++ migrations/versions/2023-11-10_04bfcb2f8f4b_initial.py
@@ -26,12 +26,10 @@ def upgrade() -> None:
     sa.Column('email', sa.String(), nullable=False),
     sa.Column('username', sa.String(), nullable=False),
     sa.Column('registered_at', sa.TIMESTAMP(), nullable=True),
-    sa.Column('role_id', sa.Integer(), nullable=True),
     sa.Column('hashed_password', sa.String(), nullable=False),
     sa.Column('is_active', sa.Boolean(), nullable=False),
     sa.Column('is_superuser', sa.Boolean(), nullable=False),
     sa.Column('is_verified', sa.Boolean(), nullable=False),
-    sa.ForeignKeyConstraint(['role_id'], ['role.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
     # ### end Alembic commands ###
